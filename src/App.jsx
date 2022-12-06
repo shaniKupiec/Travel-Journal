055b165b2982468d3665376674af4039d5818769
shani kupiec <shani.kupiec@gmail.com>
2022-12-06 17:23:54 +0200

    header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Header from './components/Header'
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <main className="app">
       <Header />
-      App
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import logo from '../../public/images/logo.png'
+
 function Header() {  
   return (
     <header className="header">
-        header
+        <img className="header__img" src={logo} alt="" />
+        <div className="header__name">my travel journal</div>
     </header>
   )
 }
